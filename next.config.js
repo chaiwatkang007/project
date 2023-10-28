2c857724e96c25452ffe48d24ea5ac45bb9e8649
chaiwatkang007 <chaiwatkang007>
2023-10-28 21:14:21 +0700

    Update 28/10/2566 R7

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['scontent.fbkk12-1.fna.fbcdn.net'], // เพิ่มโดเมนของรูปภาพที่คุณใช้ที่นี่
-  },
 }
 
 module.exports = nextConfig
